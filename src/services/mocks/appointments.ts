53ea608585059d18ee20e52b919d0706dc356de4
victormedeiros1 <josevictordev@gmail.com>
2026-04-02 02:47:41 -0300

    fix: structure of mock

--- src/services/mocks/appointments.ts
+++ src/services/mocks/appointments.ts
@@ -4,61 +4,53 @@ export const appointments: AppointmentEvent[] = [
 	{
 		start: new Date('2026-04-05T06:30:00Z'),
 		end: new Date('2026-04-05T07:30:00Z'),
-		appointment: {
-			client: {
-				name: 'João Silva'
-			},
-			service: {
-				id: 0,
-				name: 'Cabelo',
-				price: 20,
-				type: ServiceType.DEFAULT
-			}
+		client: {
+			name: 'João Silva'
+		},
+		service: {
+			id: 0,
+			name: 'Cabelo',
+			price: 20,
+			type: ServiceType.DEFAULT
 		}
 	},
 	{
 		start: new Date('2026-04-05T08:30:00Z'),
 		end: new Date('2026-04-05T09:30:00Z'),
-		appointment: {
-			client: {
-				name: 'Rogério Santos'
-			},
-			service: {
-				id: 1,
-				name: 'Barba',
-				price: 30,
-				type: ServiceType.DEFAULT
-			}
+		client: {
+			name: 'Rogério Santos'
+		},
+		service: {
+			id: 1,
+			name: 'Barba',
+			price: 30,
+			type: ServiceType.DEFAULT
 		}
 	},
 	{
 		start: new Date('2026-04-05T14:00:00Z'),
 		end: new Date('2026-04-05T15:00:00Z'),
-		appointment: {
-			client: {
-				name: 'Marcelo Carvalho'
-			},
-			service: {
-				id: 2,
-				name: 'Cabelo e Barba',
-				price: 45,
-				type: ServiceType.CUSTOM
-			}
+		client: {
+			name: 'Marcelo Carvalho'
+		},
+		service: {
+			id: 2,
+			name: 'Cabelo e Barba',
+			price: 45,
+			type: ServiceType.CUSTOM
 		}
 	},
 	{
 		start: new Date('2026-04-05T10:00:00Z'),
 		end: new Date('2026-04-05T11:00:00Z'),
-		appointment: {
-			client: {
-				name: 'Carlos Pereira'
-			},
-			service: {
-				id: 3,
-				name: 'Cabelo',
-				price: 15,
-				type: ServiceType.CUSTOM
-			}
+		client: {
+			name: 'Carlos Pereira'
+		},
+		service: {
+			id: 3,
+			name: 'Cabelo',
+			price: 15,
+			type: ServiceType.CUSTOM
 		}
 	}
 ]
