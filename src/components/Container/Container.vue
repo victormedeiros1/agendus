79e32a85576163d43c07d434d80ea817204d0681
victormedeiros1 <josevictordev@gmail.com>
2026-04-06 02:25:38 -0300

    fix: Container full width

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -15,13 +15,10 @@
 	width: 100%;
 	min-height: 100vh;
 	margin: 0 auto;
-	padding: var(--p-64) var(--p-64) var(--p-64)
-		calc(var(--sidebarDistance) + var(--p-64));
+	padding-left: calc(var(--sidebarDistance));
 
 	@media (max-width: 768px) {
 		min-height: 100svh;
-		padding: var(--p-20);
-		padding-top: var(--p-96);
 	}
 }
 </style>
