05fb87597861e6531d3921b9b2a286338e2c92ca
victormedeiros1 <josevictordev@gmail.com>
2026-04-08 01:09:41 -0300

    feat: component Modal

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { Button, Dialog } from 'primevue'
+
+const props = defineProps<{
+	isOpen: boolean
+	headerText: string
+	bodyText: string
+	confirmButtonText: string
+	cancelButtonText: string
+	confirmAction: () => void
+}>()
+
+const emit = defineEmits<{
+	(e: 'close'): void
+}>()
+
+const handleConfirmAction = (): void => {
+	props.confirmAction()
+	emit('close')
+}
+
+const handleCancelAction = (): void => {
+	emit('close')
+}
+</script>
+
+<template>
+	<Dialog
+		class="modal"
+		:header="headerText"
+		modal
+		:style="{ width: '100%', maxWidth: '600px' }"
+		:visible="isOpen"
+	>
+		<div class="modal__body">
+			<p class="modal__text">{{ bodyText }}</p>
+		</div>
+		<div class="modal__footer">
+			<Button label="Confirmar" severity="success" @click="handleConfirmAction" />
+			<Button label="Cancelar" severity="danger" @click="handleCancelAction" />
+		</div>
+	</Dialog>
+</template>
+
+<style scoped lang="scss">
+.modal {
+	&__footer {
+		display: flex;
+		justify-content: flex-end;
+		gap: var(--m-8);
+		margin-top: var(--m-32);
+	}
+}
+</style>
